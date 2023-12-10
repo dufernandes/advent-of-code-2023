4431503000c0a542322f03c19329a939c78eed34
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2023-12-10 01:29:53 +0100

    added correct version for part b

--- src/main/java/advent/of/code/year_2023/day_04/Scratchcards.java
+++ src/main/java/advent/of/code/year_2023/day_04/Scratchcards.java
@@ -24,26 +24,43 @@ public class Scratchcards {
   }
 
   public long getNumberOfScoreCards() throws IOException {
-    long sum = 0;
     InputStream is = GearRatios.class.getResourceAsStream(INPUT_FILE);
 
-    Map<String, Integer> numberOfCards = new HashMap<>();
+    Map<Integer, Integer> numberOfCards = new HashMap<>();
     try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
       String line;
       while ((line = br.readLine()) != null) {
-        String currentCard = line.split(":")[0];
+        int currentCardNumber = Integer.parseInt(line.split(":")[0].split("\\s+")[1]);
         List<String> winningNumbers = Arrays.stream(line.split("\\s+\\|\\s+")[0].split(":\\s+")[1].split("\\s+")).toList();
         List<String> myNumbers = Arrays.stream(line.split("\\s+\\|\\s+")[1].split("\\s+")).toList();
 
-        int points = 0;
-        for (String winningNumber : winningNumbers) {
-          if (myNumbers.contains(winningNumber)) {
-            points += 1;
-          }
+        incrementNumberOfCardsInMap(numberOfCards, currentCardNumber);
+
+        int points = winningNumbers.stream()
+                .reduce(0, (partialSum, wNumber)
+                        -> myNumbers.contains(wNumber) ? partialSum + 1 : partialSum,
+                        Integer::sum);
+
+        int numberOfCurrentCard = numberOfCards.get(currentCardNumber);
+
+        for (int i = currentCardNumber + 1; i <= currentCardNumber + points; i++) {
+          incrementNumberOfCardsInMap(numberOfCards, i, numberOfCurrentCard);
         }
       }
     }
-    return sum;
+    return numberOfCards.values().stream().reduce(0, Integer::sum);
+  }
+
+  private static void incrementNumberOfCardsInMap(Map<Integer, Integer> numberOfCards, int currentCard) {
+    incrementNumberOfCardsInMap(numberOfCards, currentCard, 1);
+  }
+
+  private static void incrementNumberOfCardsInMap(Map<Integer, Integer> numberOfCards, int currentCard, int incrementAmount) {
+    if (numberOfCards.containsKey(currentCard)) {
+      numberOfCards.put(currentCard, numberOfCards.get(currentCard) + incrementAmount);
+    } else {
+      numberOfCards.put(currentCard, incrementAmount);
+    }
   }
 
   public long getScratchcardsPoints() throws IOException {
